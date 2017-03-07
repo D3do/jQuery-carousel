4f30907bbfa3de659b95adb6f5ffda4d842d5a31
Sebastian Kozakiewicz <skkozakiewicz@gmail.com>
2017-03-07 22:23:31 +0100

    fixing problems

--- js/script.js
+++ js/script.js
@@ -1,47 +1,57 @@
 $(function(){ 
-	var carouselList = $("#carousel ul");
+    carouselList = $("#carousel ul");
 
-	var interval = setInterval(nextSlide, 6000);
+    var interval = setInterval(nextSlide, 4000);
 
-	function nextSlide(){
-		carouselList.animate({'marginLeft':-980}, 500, moveFirstSlide);
-	};
+    function nextSlide(){
+        carouselList.animate({'marginLeft':-980}, 500, moveFirstSlide);
+    };
 
-	function moveFirstSlide(){
-		var firstItem = carouselList.find("li:first");
-		var lastItem = carouselList.find("li:last");
+    function moveFirstSlide(){
+        var firstItem = carouselList.find("li:first");
+        var lastItem = carouselList.find("li:last");
 
-		lastItem.after(firstItem);
-		carouselList.css({marginLeft:0});
-	}
+        lastItem.after(firstItem);
+        carouselList.css({marginLeft:0});
+    }
 
-	function previousSlide() {
-		carouselList.animate({'marginLeft':980}, 500, moveLastSlide);
-	}
+    function previousSlide() {
+        carouselList.animate({'marginLeft':0}, 500, moveLastSlide);
+    }
 
-	function moveLastSlide(){
-		var firstItem = carouselList.find("li:first");
-		var lastItem = carouselList.find("li:last");
+    function moveLastSlide(){
+        var firstItem = carouselList.find("li:first");
+        var lastItem = carouselList.find("li:last");
 
-		firstItem.before(lastItem);
-		carouselList.css({marginLeft:0});
-	}
+        firstItem.before(lastItem);
+        carouselList.css({marginLeft:-980});
+    }
 
-	function resetInterval() {
-		clearInterval(interval);
-		interval = setInterval(nextSlide, 6000);
-	}
+    function resetInterval() {
+        clearInterval(interval);
+        interval = setInterval(nextSlide, 4000);
+    }
 
-	function rightArrowClick() {
-		nextSlide();
-		resetInterval();
-	}
+    function rightArrowClick() {
+        if (carouselList[0].style.marginLeft === '0px') {
+            nextSlide();
+        } else {
+            carouselList.css({marginLeft:0});
+            nextSlide();
+        }
+        resetInterval();
+    }
 
-	function leftArrowClick() {
-		previousSlide();
-		resetInterval();
-	}
+    function leftArrowClick() {
+        if (carouselList[0].style.marginLeft === '-980px') {
+            previousSlide();
+        } else {
+            carouselList.css({marginLeft:-980});
+            previousSlide();
+        }
+        resetInterval();
+    }
 
-	$('.right').on('click', rightArrowClick);
-	$('.left').on('click', leftArrowClick);
+    $('.right').on('click', rightArrowClick);
+    $('.left').on('click', leftArrowClick);
 });
